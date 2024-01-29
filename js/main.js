7f808f800ceed63ac196633e5bcd7f3972f217d3
AhmedMonem122 <ahmed.monem90@yahoo.com>
2024-01-29 21:36:19 +0200

    Add slider and counter

--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
 "use strict";
 
-const accordionButtons = Array.from(
-  document.querySelectorAll(".accordion-button")
-);
-const accordionIcons = Array.from(
-  document.querySelectorAll(".accordion__icon")
-);
+// owl carousel init
 
-accordionButtons.forEach((btn, i, arr) => {
-  btn.addEventListener("click", () => {
-    accordionIcons[i].classList.toggle("accordion__icon--is-open");
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    // loop: true,
+    // autoplay: true,
+    // autoplayTimeout: 5000,
+    // autoplayHoverPause: true,
+    dots: true,
+    nav: false,
+    // stagePadding: -5,
+    rtl: true,
+    margin: 20,
+    responsiveBaseElement: "body",
+    responsive: {
+      0: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+      },
+    },
   });
+});
+
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   autoplay: true,
+//   autoplayTimeout: 5000,
+//   autoplayHoverPause: true,
+//   dots: true,
+//   stagePadding: -5,
+//   responsiveBaseElement: "body",
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     700: {
+//       items: 2,
+//     },
+//     1220: {
+//       items: 3,
+//     },
+//   },
+// });
+
+// Initialize Counter
+
+$(".counter").counter({
+  countFrom: 1,
 });
