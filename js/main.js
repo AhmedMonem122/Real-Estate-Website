b1b5a65998c88ea0bc147737de95db15810f412b
AhmedMonem122 <ahmed.monem90@yahoo.com>
2024-02-02 20:13:34 +0200

    **

--- js/main.js
+++ js/main.js
@@ -54,3 +54,47 @@ $(document).ready(function () {
 $(".counter").counter({
   countFrom: 1,
 });
+
+// Disable the right click option
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+// Disable the keyboards shortcuts
+document.addEventListener(
+  "keydown",
+  (event) => {
+    console.log(event);
+
+    if (event.code === "F12") {
+      event.preventDefault();
+      return false;
+    } else if (
+      (event.ctrlKey || event.metaKey) &&
+      (event.shiftKey || event.altKey) &&
+      (event.code === "KeyI" || event.code === "KeyJ" || event.code === "KeyC")
+    ) {
+      event.preventDefault();
+      return false;
+    } else if ((event.ctrlKey || event.metaKey) && event.code === "KeyU") {
+      event.preventDefault();
+      return false;
+    }
+  },
+  false
+);
+
+// Disable the right click with alerts
+if (document.addEventListener) {
+  document.addEventListener(
+    "contextmenu",
+    (e) => {
+      alert("You Can not Do This!");
+      e.preventDefault();
+    },
+    false
+  );
+} else {
+  document.attachEvent("oncontextmenu", function () {
+    alert("You Can not Do This!");
+    window.event.returnValue = false;
+  });
+}
